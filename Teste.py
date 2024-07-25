1679a25bb1852f3ad9d788385e115d92f2207152
Pablo <pablo.vasques@sou.ufac.br>
2024-07-25 19:04:50 -0300

    Teste

---
+++ Teste.py
@@ -0,0 +1,12 @@
+print("Qual seu nome?")
+nome = str(input())
+print("Qual sua idade?")
+idade = int(input())
+print("Quanto é 1.0 + 0.5?")
+resposta = float(input())
+
+if (resposta == 1.5):
+    print(f'Seu nome é {nome} e sua idade é {idade}')
+else:
+    print('Você é burro meu parceiro')
+
